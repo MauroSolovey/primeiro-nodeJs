d3f826da96a54915d9a3cc884f7d879ae2afc7fa
mauro solovey <maurosolovey@gmail.com>
2021-05-13 14:09:06 -0300

    create file.js

---
+++ file.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+// fs.writeFile('test.txt', 'ola nodejs\n', err => {
+//   console.log(err);
+// })
+//
+// fs.appendFile('test.txt', 'ola nodejs\n', err => {
+//   console.log(err);
+// })
+//
+//
+// fs.rename('test.txt','testnovo.txt', err => console.log(err)); //cuidado quando existir um arquivo com mesmo nome ele apaga o arquivo
+//
+// fs.unlink('testnovo.txt', err => console.log(err));
+
+console.log(__dirname);
